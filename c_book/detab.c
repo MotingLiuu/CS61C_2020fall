c803f6a430afcdf8225350337dc69ecd93252fa8
Mutyuu <mutyuu@Mutyuus-MacBook-Air.local>
2025-09-17 13:37:25 +0800

    added detab.c

---
+++ c_book/detab.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+#define EXIT_SUCCESS 0
+#define column_num 8
+
+int main(int argc, char *argv[])
+{
+  int c;
+  while ((c = getchar()) != EOF) {
+    if (c == '\t') {
+      for (int i = 0; i < 4; i++) {
+        putchar(' ');
+      }
+    } else {
+      putchar(c);
+    }
+  }
+
+  return EXIT_SUCCESS;
+}
