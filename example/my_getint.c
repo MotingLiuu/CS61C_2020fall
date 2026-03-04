acf6f39fb11684b555ed752cffbac976ad59d6f5
MotingLiuu <moting.liuu@gmail.com>
2026-03-05 02:52:07 +0900

    finished e5.2

--- example/my_getint.c
+++ example/my_getint.c
@@ -33,13 +33,62 @@ int getint(int *pn) {
         return 0;
     }
     sign = (c == '-') ? -1 : 1;
-    if (c == '+' || c == '-') 
-        c = getch();
+    if (c == '+' || c == '-') {
+        int c2 = getch();
+        if (!isdigit(c2)) {
+            if (c2 != EOF) {
+                ungetch(c2);
+            }
+            ungetch(c);
+            return 0;
+        } else {
+            c = c2;
+        }
+    }
     for (*pn = 0; isdigit(c); c = getch())
          *pn = 10 * *pn + (c - '0');
     *pn *= sign;
     if (c != EOF)
         ungetch(c);
+    return c;
+}
+
+int getfloat(double *pd) {
+    int c, sign;
+    while (isspace(c = getch()))
+        ;
+    if (!isdigit(c) && c != '+' && c != '-' && c != '.' && c != EOF) {
+        ungetch(c);
+        return 0;
+    }
+    sign = (c == '-') ? -1 : 1;
+    if (c == '+' || c == '-') {
+        int c2 = getch();
+        if (!isdigit(c2) && c2 != '.') {
+            if (c2 != EOF) {
+                ungetch(c2);
+            } 
+            ungetch(c);
+            return 0;
+        } else {
+            c = c2;
+        }
+    }
+    for (*pd = 0; isdigit(c); c = getch()) {
+        *pd = 10 * *pd + c - '0';
+    }
+    if (c == '.') {
+        double dev = 1;
+        for (;isdigit(c = getch());) {
+            *pd = 10 * *pd + c - '0';
+            dev = 10 * dev;
+        }
+        *pd = *pd / dev;
+    }
+    *pd = *pd * sign;
+    if (c != EOF) {
+        ungetch(c);
+    }
     return c;
 }
 
