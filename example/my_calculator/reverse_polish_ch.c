be5266a5cc708cec9734e82103fde9c18f415926
MotingLiuu <moting.liuu@gmail.com>
2026-03-04 18:13:49 +0900

    updated reverse_polish_op.c

--- example/my_calculator/reverse_polish_ch.c
+++ example/my_calculator/reverse_polish_ch.c
@@ -1,5 +1,6 @@
 #define BUFSIZE 100
 #include <stdio.h>
+#include <stdbool.h>
 
 int buf[BUFSIZE]; // buffer for ungetch
 int bufp = 0;      // next free position in buf
@@ -7,9 +8,18 @@ int bufp = 0;      // next free position in buf
 int getch(void) { return (bufp > 0) ? buf[--bufp] : getchar(); }
 
 void ungetch(int c) {
+  if (c == EOF) {
+    return;
+  }
   if (bufp >= BUFSIZE) {
     printf("ungetch: too many characters\n");
   } else {
     buf[bufp++] = c;
+  }
+}
+
+void ungets(char *s) {
+  for(int i = 0; s[i] != '\0'; i++) {
+    ungetch(s[i]);
   }
 }
