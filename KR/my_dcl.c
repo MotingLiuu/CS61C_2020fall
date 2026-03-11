24675e5ddfa66e156e24f10cbdc268aa81917c33
MotingLiuu <moting.liuu@gmail.com>
2026-03-11 22:33:29 +0900

    added examples of KR

---
+++ KR/my_dcl.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#define MAXTOKEN 100
+enum { NAME, PARENS, BRACKETS };
+void dcl(void);
+void dirdcl(void);
+int gettoken(void);
+int tokentype; /* type of last token */
+char token[MAXTOKEN]; /* last token string */
+char name[MAXTOKEN]; /* identifier name */
+char datatype[MAXTOKEN]; /* data type = char, int, etc. */
+char out[1000];
+
+int main(int argc, char *argv[]) {
+    while (gettoken() != EOF) {
+        strcpy(datatype, token);
+        out[0] = '\0';
+        dcl();
+        if (tokentype != '\n')
+            printf("syntax error\n");
+        printf("%s: %s %s\n", name, out, datatype);
+    }
+    return 0;
+}
