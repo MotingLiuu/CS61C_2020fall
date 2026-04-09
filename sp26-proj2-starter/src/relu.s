a631273db70e71f49710e780283791e7ed4664b0
MotingLiuu <moting.liuu@gmail.com>
2026-04-09 16:46:09 +0800

    completed relu.s

--- sp26-proj2-starter/src/relu.s
+++ sp26-proj2-starter/src/relu.s
@@ -14,25 +14,27 @@
 # ==============================================================================
 relu:
     # Prologue
-
+    bge x0 a1 malformed
+    li t0 4
+    mul t0 t0 a1
+    add t0 t0 a0
 
 loop_start:
-
-
-
-
-
-
-
+    blt x0 a1 loop_continue
+    j loop_end
 
 loop_continue:
-
-
+    addi t0 t0 -4
+    addi a1 a1 -1
+    lw t1 0(t0)
+    bge t1 x0 loop_start
+    sw x0 0(t0)
+    j loop_start
 
 loop_end:
-
-
     # Epilogue
-
-
     jr ra
+
+malformed:
+    li a0 36
+    j exit
