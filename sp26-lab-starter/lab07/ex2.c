ef97705cee6f1cc0f2e9cd2861734f7fef74d4a2
MotingLiuu <moting.liuu@gmail.com>
2026-06-10 03:11:37 +0800

    finished lab07

--- sp26-lab-starter/lab07/ex2.c
+++ sp26-lab-starter/lab07/ex2.c
@@ -12,6 +12,12 @@ double dotp_critical(double* x, double* y, int arr_size) {
     double global_sum = 0.0;
     // TODO: Implement this function
     // Use the critical keyword here!
+    // 
+    #pragma omp critical
+    {
+        for (int i = 0; i < arr_size; i++)
+            global_sum += x[i] * y[i];
+    }
     return global_sum;
 }
 
@@ -20,6 +26,11 @@ double dotp_reduction(double* x, double* y, int arr_size) {
     double global_sum = 0.0;
     // TODO: Implement this function
     // Use the reduction keyword here!
+    // 
+    #pragma omp parallel for reduction(+:global_sum)
+    for (int i = 0; i < arr_size; i++) {
+        global_sum += x[i] * y[i];
+    }
     return global_sum;
 }
 
@@ -28,11 +39,22 @@ double dotp_manual_reduction(double* x, double* y, int arr_size) {
     double global_sum = 0.0;
     // TODO: Implement this function
     // Do NOT use the `reduction` directive here!
+    // 
     #pragma omp parallel
     {
-        double local_sum = 0.0;
         // TODO: Parallel Section
+        // 
+        int num_threads = omp_get_num_threads();
+        int tid = omp_get_thread_num();
+        double local_sum = 0.0;
+        for (int i = tid; i < arr_size; i += num_threads) {
+            local_sum += x[i] * y[i];
+        }
 
+        #pragma omp critical
+        {
+            global_sum += local_sum;
+        }
         // TODO: Critical Section
     }
     return global_sum;
